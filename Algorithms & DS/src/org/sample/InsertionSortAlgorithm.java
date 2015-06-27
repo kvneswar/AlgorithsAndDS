8b0f01e45758b58183a2f5b8ddcd27e0a009e6dd
Eswar <kvn.eswar@hotmail.com>
2015-06-27 01:35:10 -0600

    Initial Commit fot insertion sort

---
+++ Algorithms & DS/src/org/sample/InsertionSortAlgorithm.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+public class InsertionSortAlgorithm {
+
+    public static void main(String[] args) {
+
+	int[] intArray = {2, 5, 8, 3};
+	int current=0;
+	int j=0;
+	for(int i=0; i<intArray.length;i++){
+	    current=intArray[i];
+	    j=i-1;
+	    while(j>=0&&intArray[j]>current){
+		intArray[j+1]=intArray[j];
+		j=j-1;
+		intArray[j+1]=current;
+	    }
+	}
+	System.out.println(intArray);
+	
+    }
+
+}
