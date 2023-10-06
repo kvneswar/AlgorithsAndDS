8f65de52f4e14965ef6785730a24b8e321e6d17f
Eswar Karumuri <kvn.eswar@hotmail.com>
2023-10-06 18:56:40 +0000

    updated the I/P

--- Algorithms & DS/src/org/sample/Factorial.java
+++ Algorithms & DS/src/org/sample/Factorial.java
@@ -3,13 +3,13 @@ package org.sample;
 public class Factorial {
 
     public static void main(String[] args) {
-	System.out.println(factorial(3));
+	System.out.println(factorial(4));
 	
     }
     
     public static int factorial(int num){
 	if(num==0) return 1;
-	return num*factorial(num-1);
+	    return num*factorial(num-1);
     }
 
 }
